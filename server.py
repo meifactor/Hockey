04f737891592a88a689a4eb2dd741b2918cae0b4
Meitalia Factor <meitalia@Meitalias-MacBook-Air.local>
2024-11-23 18:32:47 -0500

    Meitalia Factor mef2241

--- server.py
+++ server.py
@@ -51,6 +51,8 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    #code added to sort scoreboard in non-increasing order
+    scoreboard.sort(key=lambda x: x['score'], reverse=True)
     return jsonify(scoreboard=scoreboard)
 
 
